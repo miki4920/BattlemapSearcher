40839d6f850c38c2828c5688169f48462b70237a
Nicolas Grobelny <miki4920@gmail.com>
2021-04-14 22:48:08 +0100

    Added a basic tokeniser TODO: Expand upon it, separate by tag and source

--- mapviewer/forms.py
+++ mapviewer/forms.py
@@ -1,5 +1,13 @@
+import re
+
 from django import forms
+from django.core.exceptions import ValidationError
 
 
 class SearchForm(forms.Form):
     text = forms.CharField(label="", help_text="", widget=forms.TextInput(attrs={'placeholder': 'Search...', 'autocomplete': 'off'}))
+
+    def clean(self, *args):
+        cleaned_data = super().clean()
+        text = cleaned_data.get("text")
+
